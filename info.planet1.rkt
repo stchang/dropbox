bec6d792e5d69d7646cc88323dc8a72abec3358e
Stephen Chang <stchang@ccs.neu.edu>
2013-01-09 17:22:46 -0500

    reset info.rkt

---
+++ info.planet1.rkt
@@ -0,0 +1,12 @@
+#lang setup/infotab
+(define name "dropbox")
+(define scribblings '(("dropbox.scrbl")))
+(define categories '(misc))
+(define repositories (list "4.x"))
+(define primary-file 
+  '("dropbox.rkt"))
+(define blurb
+  (list '(div "Racket bindings for Dropbox API.")))
+(define release-notes 
+  (list
+   '(div "")))
