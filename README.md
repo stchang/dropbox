e5f3f506cccce65d6883aa671c6fac880a998f18
Stephen Chang <stchang@ccs.neu.edu>
2013-01-16 15:27:29 -0500

    update info.rkt and readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ racket-dropbox
 
 Racket bindings for Dropbox API.
 
-[documentation](http://planet.racket-lang.org/package-source/stchang/dropbox.plt/1/1/planet-docs/dropbox/index.html)
+[documentation](http://planet.racket-lang.org/package-source/stchang/dropbox.plt/2/0/planet-docs/dropbox/index.html)
 
 Racket can be downloaded [here](http://racket-lang.org/). (Requires Racket 5.3.1.12 or later.)
 
--- dropbox/info.rkt
+++ dropbox/info.rkt
@@ -5,6 +5,8 @@
 (define repositories (list "4.x"))
 (define primary-file '("dropbox.rkt"))
 (define required-core-version "5.3.1.12")
-(define version "1.1")
+(define version "2.0")
 (define blurb (list '(div "Racket bindings for Dropbox API.")))
-(define release-notes  (list '(div "")))
+(define release-notes  
+  (list '(div "2.0: Remove upload-file and rename upload-large-file to upload-file.")
+        '(div "Combine obtain-request-token and get-authorization-url and remove obtain-request-token.")))
